2ec16b6834832feb4f0b8ac22c64f232a9e098df
Eric Esteves Martins <estevesmartinseric@gmail.com>
2021-07-16 15:06:40 -0300

    entrega

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function counter (){
    typedText = typedText.toLowerCase(); 
    // Isto muda todas as letras para minúsculas
    typedText = typedText.replace(/[^a-z'\s]+/g, ""); 
-   typedText = typedText.replace(/" "/i, ""); 
    // typedText = typedText.replace(/'/n'/i, ""); 
 
    // Isso se livra de todos os caracteres exceto letras comuns, espaços e apóstrofos. 
@@ -19,10 +18,7 @@ function counter (){
          letterCounts[currentLetter]++; 
       }
    }
-   console.log(typedText)
-
-   console.log(letterCounts)
-      for (let letter in letterCounts) { 
+   for (let letter in letterCounts) { 
       const span = document.createElement("span"); 
       const textContent = `"${letter}": ${letterCounts[letter]}, `;
       span.innerText = textContent; 
@@ -30,7 +26,30 @@ function counter (){
       letters.appendChild(span); 
    }
 
-   return letterCounts
+
+   let typedTextwords = document.getElementById("textInput").value
+   typedTextwords = typedTextwords.toLowerCase(); 
+   typedTextwords = typedTextwords.replace(/[^a-z'\s]+/g, ""); 
+   words = typedTextwords.split(/\s/);
+   //quebra nos espaços
+   const wordcounts = {};
+   for (let i = 0; i < words.length; i++) {
+      currentWord = words[i];
+      // faça algo com cada letra 
+      if (wordcounts[currentWord] === undefined) {
+         wordcounts[currentWord] = 1; 
+      } 
+      else { 
+         wordcounts[currentWord]++; 
+      }
+   }
+   for (let word in wordcounts) { 
+      const spanWord = document.createElement("span"); 
+      const wordContent = `"${word}": ${wordcounts[word]}, `;
+      spanWord.innerText = wordContent; 
+      const wordsplace = document.getElementById("wordsDiv");
+      wordsplace.appendChild(spanWord); 
+   }
 }
 
 const button = document.getElementById("countButton");
@@ -42,6 +61,9 @@ button.addEventListener("click", function() {
 
 
 
+
+
+
    // for (var prop in letterCounts) {
    //    const lettersDiv = document.getElementById("lettersDiv")
    //    let letterCount = document.createElement('p')
@@ -50,9 +72,4 @@ button.addEventListener("click", function() {
    // }
 
 
-
-
-
-// words = typedText.split(/\s/);
-// //quebra nos espaços
 
